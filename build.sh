e3eb2d1b6b57ce242e07cd9919fce9b462e14189
fatterbetter <337521002@qq.com>
2019-11-15 14:33:18 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,9 +9,9 @@ fi
 
 [ ! -e "Dockerfile" ] && echo "Missing Dockerfile" && exit 2
 
-docker build -t mdns/sshd:$1 .
+docker build -t fatterbetter/sshd:$1 .
 
 if [ "$2" = "push" ]
 then
-	docker push mdns/sshd:$1
+	docker push fatterbetter/sshd:$1
 fi
